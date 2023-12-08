9e4231418c91d31dc4bd649f517d685403eb1db1
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-08 15:06:57 +0100

    renamed folder to faiss index

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .faiss_database.router import router as faiss_router
+from .faiss_index.router import router as faiss_router
 
 app = FastAPI()
 app.include_router(faiss_router)
