6f9c637fa21ef39cbe99529e1c9f0f9d29337288
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-08 15:01:04 +0100

    moved auth logic to dependencies file

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,13 +1,13 @@
-from decouple import config
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from .config import FAISS_USER, FAISS_PASSWORD
+
 
 security = HTTPBasic()
 
+
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = config("FAISS_USER")
-    correct_password = config("FAISS_PASSWORD")
-    if credentials.username != correct_username or credentials.password != correct_password:
+    if credentials.username != FAISS_USER or credentials.password != FAISS_PASSWORD:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect credentials",
