2bbc44a9ba8721f477f0866e17debb86b9cbff40
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-08 13:36:19 +0100

    switched to python-decouple

--- projecttextfaiss/auth.py
+++ projecttextfaiss/auth.py
@@ -1,11 +1,12 @@
+from decouple import config
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 security = HTTPBasic()
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = os.getenv("FAISS_USER")
-    correct_password = os.getenv("FAISS_PASSWORD")
+    correct_username = config("FAISS_USER")
+    correct_password = config("FAISS_PASSWORD")
     if credentials.username != correct_username or credentials.password != correct_password:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
