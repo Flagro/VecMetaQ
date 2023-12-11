8f4c9be1f260dd422d0caf34d35fc10fe554ed8c
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-11 03:22:44 +0100

    implimented chunking for vector storage

--- src/vector_storage/vector_storage.py
+++ src/vector_storage/vector_storage.py
@@ -1,3 +1,22 @@
+def chunk_text(text, max_tokens=512):
+    # Split text into chunks of max_tokens
+    chunks = []
+    current_chunk = ""
+    for token in text.split(" "):
+        if len(current_chunk) + len(token) < max_tokens:
+            current_chunk += token + " "
+        else:
+            chunks.append(current_chunk)
+            current_chunk = ""
+    chunks.append(current_chunk)
+    return chunks
+
+
+def concat_chunks(chunks):
+    # Concatenate chunks into single string
+    return "".join(chunks)
+
+
 class VectorStorage:
     def __init__(self, faiss_index, embedding_model, metadata_db):
         self.faiss_index = faiss_index
@@ -5,26 +24,29 @@ class VectorStorage:
         self.metadata_db = metadata_db
 
     def add_data(self, text, file_path, metadata):
-        # Generate embedding
-        embedding = self.embedding_model.encode(text)
-
-        # Add embedding to FAISS index
-        index_id = self.faiss_index.add_vector(embedding)
-
-        # Store metadata in database with index_id
-        self.metadata_db.add_metadata(index_id, file_path, metadata)
+        for chunk in chunk_text(text, self.embedding_model.max_tokens):
+            embedding = self.embedding_model.generate_embedding(chunk)
+            index_id = self.faiss_index.add_vector(embedding)
+            self.metadata_db.add_metadata(index_id, file_path, metadata)
 
     def delete_data(self, file_path):
         # Mark entries as deleted in metadata database for given file_path
         self.metadata_db.mark_deleted(file_path)
 
     def search_similar(self, query_text):
-        # Generate embedding for query
-        query_embedding = self.embedding_model.encode(query_text)
-
-        # Search in FAISS
-        distances, indices = self.faiss_index.search_vector(query_embedding)
-
-        # Fetch metadata for indices and aggregate by file_path
+        query_embedding = self.embedding_model.generate_embedding(query_text)
+        indices = self.faiss_index.search_vector(query_embedding)
         results = self.metadata_db.get_metadata(indices, exclude_deleted=True)
-        return distances, results
+
+        # Aggregate results by file_path and concatenate text chunks
+        aggregated_results = {}
+        for result in results:
+            file_path = result['file_path']
+            if file_path not in aggregated_results:
+                aggregated_results[file_path] = {'text': [], 'file_path': file_path, 'metadata': result['metadata']}
+            aggregated_results[file_path]['text'].append(result['text'])
+        
+        for file_path, result in aggregated_results.items():
+            result['text'] = concat_chunks(result['text'])
+
+        return list(aggregated_results.values())
