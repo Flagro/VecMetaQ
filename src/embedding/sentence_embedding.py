7190236a153ac92618c7269048e146b4348be772
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-11 03:32:44 +0100

    max tokens field for embedding model

--- src/embedding/sentence_embedding.py
+++ src/embedding/sentence_embedding.py
@@ -1,10 +1,11 @@
 from sentence_transformers import SentenceTransformer
-from .config import SENTENCE_TRANSFORMERS_MODEL
+from .config import SENTENCE_TRANSFORMERS_MODEL, SENTENCE_TRANSFORMERS_MAX_TOKENS
 
 
 class SentenceEmbedding:
     def __init__(self):
         self.model = SentenceTransformer(SENTENCE_TRANSFORMERS_MODEL)
+        self.max_tokens = SENTENCE_TRANSFORMERS_MAX_TOKENS
 
     def encode(self, doc):
         return self.model.encode(doc)
