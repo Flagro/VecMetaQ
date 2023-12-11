0760982e2d24b5d0f125a713227d4b5c92881c37
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-11 03:49:19 +0100

    filtering distances by threshold

--- src/faiss_index/faiss_index.py
+++ src/faiss_index/faiss_index.py
@@ -15,4 +15,12 @@ class FaissIndex:
     def search_vector(self, embedding, k=FAISS_DEFAULT_K, distance_threshold=FAISS_DEFAULT_DISTANCE_THRESHOLD):
         vec = np.array(embedding, dtype='float32').reshape(1, -1)
         faiss.normalize_L2(vec)
-        return self.index.search(vec, k)
+        distances, indices = self.index.search(vec, k)
+
+        # Apply distance threshold
+        filtered_indices = []
+        for dist, idx in zip(distances[0], indices[0]):
+            if dist < distance_threshold:
+                filtered_indices.append(idx)
+
+        return filtered_indices
